a69a7e0cf6777fbf5cf460005dbc2291a47e2195
xdedss <liuzhuoran_2000@163.com>
2023-05-03 20:20:15 +0800

    move to new directory

--- bboxi.py
+++ bboxi.py
@@ -7,7 +7,7 @@ import http.server, socketserver
 
 from typing import Sequence
 
-TEMPLATE_DIR = 'template'
+TEMPLATE_DIR = 'bbox_webui_template'
 
 class ImageData():
 
@@ -45,38 +45,6 @@ class ImageData():
 def get_port_temp_dir(port):
     return f'temp_{port}'
 
-def generate_from_template(data, class_names, class_colors, port):
-    
-    d = get_port_temp_dir(port)
-
-    if (os.path.exists(d)):
-        shutil.rmtree(d)
-
-    shutil.copytree(TEMPLATE_DIR, d)
-
-    # generate imageList json
-    img_list = []
-    for i, image_data in enumerate(data):
-        assert isinstance(image_data, ImageData)
-        img = image_data.img
-        fname = f'{i:03d}.jpg'
-        name = image_data.name
-        if (name is None):
-            name = f'{i:03d}'
-        cv2.imwrite(os.path.join(d, fname), img)
-        img_list.append({
-            'file': fname,
-            'name': name,
-            'bbox': image_data.bboxes,
-        })
-
-    js_file = os.path.join(d, 'inject.js')
-    with open(js_file, 'r', encoding='utf-8') as f:
-        s = f.read()
-    s = s % (json.dumps(img_list), json.dumps(class_names), json.dumps(class_colors))
-    with open(js_file, 'w', encoding='utf-8') as f:
-        f.write(s)
-
 def serve_blocking(port, http_root):
     class Handler(http.server.SimpleHTTPRequestHandler):
         def __init__(self, *args, **kwargs):
@@ -90,6 +58,7 @@ def serve_blocking(port, http_root):
         httpd.serve_forever()
 
     print(f"Serving HTTP server on port {port} with root directory '{http_root}'...")
+    print(f'http://127.0.0.1:{port}/app.html')
     
     thread = threading.Thread(target=serve_run, daemon=True)
     thread.start()
@@ -103,6 +72,35 @@ def serve_blocking(port, http_root):
     print("HTTP server stopped.")
     
 
+def visualize_generate_html(data, class_names, class_colors, save_dir):
+    assert not os.path.exists(save_dir), save_dir + ' already exists!'
+
+    shutil.copytree(TEMPLATE_DIR, save_dir)
+
+    # generate imageList json
+    img_list = []
+    for i, image_data in enumerate(data):
+        assert isinstance(image_data, ImageData)
+        img = image_data.img
+        fname = f'{i:03d}.jpg'
+        name = image_data.name
+        if (name is None):
+            name = f'{i:03d}'
+        cv2.imwrite(os.path.join(save_dir, fname), img)
+        img_list.append({
+            'file': fname,
+            'name': name,
+            'bbox': image_data.bboxes,
+        })
+
+    js_file = os.path.join(save_dir, 'inject.js')
+    with open(js_file, 'r', encoding='utf-8') as f:
+        s = f.read()
+    s = s % (json.dumps(img_list), json.dumps(class_names), json.dumps(class_colors))
+    with open(js_file, 'w', encoding='utf-8') as f:
+        f.write(s)
+
+
 def visualize_start_server(data, class_names, class_colors, port):
     '''
     data: list of ImageData
@@ -110,9 +108,13 @@ def visualize_start_server(data, class_names, class_colors, port):
     class_colors: list of [r, g, b] (0-255)
     port: int
     '''
+    
+    d = get_port_temp_dir(port)
 
-
-    generate_from_template(data, class_names, class_colors, port)
+    if (os.path.exists(d)):
+        shutil.rmtree(d)
+    
+    visualize_generate_html(data, class_names, class_colors, d)
     serve_blocking(port, get_port_temp_dir(port))
     shutil.rmtree(get_port_temp_dir(port))
     
@@ -123,23 +125,28 @@ def test():
     tags = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
     classes = ['dog', 'cat', 'person', 'car', 'plane']
     colors = [
-        [255, 80, 80],      # Brighter Red
-        [80, 255, 80],      # Brighter Green
-        [80, 80, 255],      # Brighter Blue
-        [255, 255, 100],    # Brighter Yellow
-        [255, 80, 255]      # Brighter Magenta
+        [255, 80, 80],      # Bright Red
+        [80, 255, 80],      # Bright Green
+        [80, 80, 255],      # Bright Blue
+        [255, 255, 100],    # Bright Yellow
+        [255, 80, 255]      # Bright Magenta
     ]
     for i in range(5):
+        # random image
         img = np.clip(np.random.randn(256, 256, 3) * 0.2 * 255, 0, 255).astype(np.uint8)
         img_data = ImageData(img)
         for j in range(10):
+            # random bbox
             cx, cy = np.random.uniform(0, img.shape[1]), np.random.uniform(0, img.shape[0])
             w, h = np.exp(np.random.uniform(np.log(5), np.log(200))), np.exp(np.random.uniform(np.log(5), np.log(200)))
+            # add
             img_data.add_bbox((cx - w/2, cy - h/2), (cx + w/2, cy + h/2), random.randint(0, len(classes) - 1), random.sample(tags, 2))
+            
             cv2.ellipse(img, (int(cx), int(cy)), (int(w/2), int(h/2)), 0, 0, 360, color=np.random.randint(0, 255, 3).tolist(), thickness=-1)
         data.append(img_data)
     visualize_start_server(data, classes, colors, 8080)
 
-test()
+if __name__ == '__main__':
+    test()
 
 
---
+++ readme.md
@@ -0,0 +1,6 @@
+
+
+`visualize_generate_html`: 只在本地生成html页面，可以用浏览器打开
+
+`visualize_start_server`: 阻塞并开启http server，可以远程访问
+
